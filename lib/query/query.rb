91cd9149c23f1e6503c6403cce52311a73bfc5bb
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-01-11 09:00:02 +0100

    added documentation to base class in query module

--- lib/query/query.rb
+++ lib/query/query.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-24 12:53:57
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-01-10 09:10:36
+# @Last Modified time: 2016-01-11 08:59:37
 
 # this module holds the classes and methods for queries regarding the data
 module Query
@@ -21,6 +21,12 @@ module Query
   # @raise [NotImplementedError] if the mentioned methods are not implemented
   class Base
 
+    # method to collect all tasks of a person in a given time interval
+    # @param [Integer] id the id of the person
+    # @param [Integer] year the requested year
+    # @param [Integer] time_delta the requested time interval
+    # @return [Hash] a hash with all tasks mapped on how the occur in the
+    #   time interval
     def self.get_data(id, year, time_delta)
       all_task = Query.data.get_tasks_to_person(id)
       tasks = {
@@ -38,6 +44,11 @@ module Query
       return tasks
     end
 
+    # method to calculate the work time in the given time period
+    # @param [Integer] id the id of the person
+    # @param [Integer] year the requested year
+    # @param [Integer] interval the requested time scale
+    # @param [Integer] time_frame the time of the interval in hours
     def self.get_interval_worktime(id, year, interval, time_frame)
       tasks = get_data(id, year, interval)
 
@@ -52,6 +63,12 @@ module Query
 
     end
 
+    # method to collect all task that occur during the time interval
+    # @param [Hash] date_values a hash containing the start time and the
+    #   end time of the requested time interval
+    # @param [Array] all_task the task with start and/ or end within the
+    #   requested time interval
+    # @return [Hash] all tasks taking place during the given time frame
     def self.collect_tasks_during(date_values, all_task)
       all_task.select { |task|
         task.start_time >= date_values[:actual] &&
@@ -59,6 +76,13 @@ module Query
       }
     end
 
+    # method to collect all task that start before the time interval but do
+    # not end in the time interval
+    # @param [Hash] date_values a hash containing the start time and the
+    #   end time of the requested time interval
+    # @param [Array] all_task the task with start and/ or end within the
+    #   requested time interval
+    # @return [Hash] all tasks running over the given time frame
     def self.collect_tasks_over(date_values, all_task)
       all_task.select { |task|
         task.start_time < date_values[:actual] &&
@@ -66,6 +90,13 @@ module Query
       }
     end
 
+    # method to collect all task that end in the time interval but were
+    # started before the requested interval
+    # @param [Hash] date_values a hash containing the start time and the
+    #   end time of the requested time interval
+    # @param [Array] all_task the task with start and/ or end within the
+    #   requested time interval
+    # @return [Hash] all tasks ending in the given time frame
     def self.collect_tasks_into(date_values, all_task)
       all_task.select { |task|
         task.start_time < date_values[:actual] &&
@@ -74,6 +105,13 @@ module Query
       }
     end
 
+    # method to collect all task that start in the time interval but do not
+    # end in the requested interval
+    # @param [Hash] date_values a hash containing the start time and the
+    #   end time of the requested time interval
+    # @param [Array] all_task the task with start and/ or end within the
+    #   requested time interval
+    # @return [Hash] all tasks starting in the given time frame
     def self.collect_tasks_beyond(date_values, all_task)
       all_task.select { |task|
         task.start_time > date_values[:actual] &&
@@ -82,8 +120,12 @@ module Query
       }
     end
 
+    # method to calculate the working hours of tasks occurring during the
+    # requested time interval
+    # @param [Array] tasks the tasks occurring during the time interval
+    # @return [Float] the sum of the working hours
     def self.get_hours_during(tasks)
-      total = 0
+      total = 0.0
       tasks.each { |task|
         total += task.end_time - task.start_time
       }
@@ -91,8 +133,12 @@ module Query
       total = (total / 3600).round(2)
     end
 
+    # method to calculate the working hours of tasks ending during the
+    # requested time interval, but starting before
+    # @param [Array] tasks the tasks ending during the time interval
+    # @return [Float] the sum of the working hours
     def self.get_into_value(tasks, time_frame)
-      total = 0
+      total = 0.0
       tasks.each { |task|
         total += task.end_time - time_frame
       }
@@ -100,8 +146,12 @@ module Query
       total = (total / 3600).round(2)
     end
 
+    # method to calculate the working hours of tasks starting during the
+    # requested time interval, but ending after
+    # @param [Array] tasks the tasks starting during the time interval
+    # @return [Float] the sum of the working hours
     def self.get_beyond_value(tasks, next_time_frame)
-      total = 0
+      total = 0.0
       tasks.each { |task|
         total += next_time_frame - task.start_time
       }
