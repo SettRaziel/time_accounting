86da3434c133aab4e5580df891216fcc4cd00fa6
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-01-29 21:09:04 +0100

    added time_query to query module

---
+++ lib/query/time_query.rb
@@ -0,0 +1,28 @@
+# @Author: Benjamin Held
+# @Date:   2016-01-23 20:07:29
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2016-01-29 21:05:06
+
+module Query
+
+  # statistic class to calculate to work time for a given month and year
+  class TimeQuery < Base
+
+    # method to retrieve the worktime for a person for the given time
+    # interval
+    def self.get_time_worktime(id, start_time, end_time)
+      times = calculate_start_and_end_time(start_time, end_time)
+      get_interval_worktime(id, times, (end_time - start_time) / 60 / 60 / 24.0)
+    end
+
+    # method to calculate the time boundaries for the given interval
+    # @param [Time] start_time the start time of the interval
+    # @param [Time] end_time the end time of the interval
+    # @return [Hash] a hash containing the two time boundaries
+    def self.calculate_start_and_end_time(start_time, end_time)
+      {:actual => start_time, :next => end_time}
+    end
+
+  end
+
+end
