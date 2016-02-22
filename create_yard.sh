d9f70809cccc0aa81665430de2a6e7a2e75ec34c
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-02-22 20:04:52 +0100

    added script for documentation

---
+++ create_yard.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @Author: Benjamin Held
+# @Date:   2015-08-30 09:48:37
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2015-09-21 13:14:55
+
+exec yard doc --private --readme ../README.md
