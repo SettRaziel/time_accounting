acef8be59f70e086bc84a06cd605c9ce7bd2839f
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-03-02 20:13:38 +0100

    extended menu options

--- lib/input/database_option.rb
+++ lib/input/database_option.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-27 12:21:25
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-02-18 18:26:47
+# @Last Modified time: 2016-03-02 20:10:49
 
 module Input
 
@@ -18,7 +18,7 @@ module Input
       while (true)
         begin
           print_menu
-          input = get_entry("Input (1-4): ").to_i
+          input = get_entry('Input (1-4): ').to_i
 
           process_input(input)
         rescue StandardError => e
@@ -33,9 +33,10 @@ module Input
     def self.print_menu
       puts "\nDatabase options"
       puts ' (1) Add entity.'
-      puts ' (2) Query entities'
-      puts ' (3) Save and exit.'
-      puts ' (4) Abort and exit.'
+      puts ' (2) Query entities.'
+      puts ' (3) Query worktime.'
+      puts ' (4) Save and exit.'
+      puts ' (5) Abort and exit.'
     end
 
     # method to process the provided input
@@ -44,8 +45,9 @@ module Input
       case input
         when 1 then EntityAddition.entity_addition_menu
         when 2 then EntityQueries.entity_query_menu
-        when 3 then save_and_exit
-        when 4 then Input.exit_script
+        when 3 then WorktimeQueries.worktime_query_menu
+        when 4 then save_and_exit
+        when 5 then Input.exit_script
       else
         puts "Error: #{input} ist not valid.".red
       end
@@ -57,7 +59,7 @@ module Input
         Input.data_handler.save_repository
         Input.exit_script
       rescue IOError => e
-        raise IOError, "Error while saving data: ".concat(e.message).red
+        raise IOError, 'Error while saving data: '.concat(e.message).red
       end
     end
 
