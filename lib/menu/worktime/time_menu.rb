c00d539c4e8057c1f9aa575428c77d0ec5cb80fc
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-04-13 17:33:40 +0200

    added documentation

--- lib/menu/worktime/time_menu.rb
+++ lib/menu/worktime/time_menu.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-03-25 12:22:05
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-04-05 17:38:55
+# @Last Modified time: 2016-04-13 17:32:19
 
 module Menu
 
@@ -43,7 +43,7 @@ module Menu
       def self.process_input(input)
         case input
           when 1 then print_all_tasks
-          when 2 then calculate_worktime
+          when 2 then retrieve_worktime
           when 3 then print_tasks_in_interval
           when 4
             @values = nil
@@ -54,18 +54,25 @@ module Menu
         return true
       end
 
+      # abstract method to retrieve the tasks for the given interval
+      # @abstract subclasses need to implement this method
+      # @raise [NotImplementedError] if the subclass does not have this method
       def self.retrieve_tasks
         fail NotImplementedError, " Error: the subclass "
         "#{self.name.split('::').last} needs to implement the method: " \
         "retrieve_tasks from its parent class".red
       end
 
+      # abstract method to retrieve the worktime for the given interval
+      # @abstract subclasses need to implement this method
+      # @raise [NotImplementedError] if the subclass does not have this method
       def self.retrieve_worktime
         fail NotImplementedError, " Error: the subclass "
         "#{self.name.split('::').last} needs to implement the method: " \
         "retrieve_worktime from its parent class".red
       end
 
+      # method to check if the necessary input was already collected
       def self.check_attributes
         if (@values == nil)
           puts "Specify necessary informations: "
@@ -74,6 +81,8 @@ module Menu
         check_time_string
       end
 
+      # method check, if the subclass has set a value for the class attribute
+      # @raise [NotImplementedError] if the subclass does not have set the value
       def self.check_time_string
         if @time_string.eql?(nil)
           fail NotImplementedError, " Error: the subclass "
@@ -82,6 +91,7 @@ module Menu
         end
       end
 
+      # method to print all the collected tasks
       def self.print_all_tasks
         tasks = retrieve_tasks
         tasks.each_key { |key|
@@ -89,16 +99,15 @@ module Menu
         }
       end
 
+      # method to print all tasks collected to a specific key
+      # @param [Array] an array containing all tasks of a given key
       def self.print_tasks_to_key(tasks)
         tasks.each { |task|
           puts task.to_string
         }
       end
 
-      def self.calculate_worktime
-        retrieve_worktime
-      end
-
+      # method to print all tasks in the given time interval
       def self.print_tasks_in_interval
         tasks = retrieve_tasks
         print_tasks_to_key(tasks[:during])
