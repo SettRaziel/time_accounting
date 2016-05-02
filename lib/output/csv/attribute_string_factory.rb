79c380ea061731c4a99d5ae4466b971cbc1160a7
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-05-02 15:03:05 +0200

    added factory class for csv output

---
+++ lib/output/csv/attribute_string_factory.rb
@@ -0,0 +1,31 @@
+# @Author: Benjamin Held
+# @Date:   2016-05-02 14:28:45
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2016-05-02 15:01:28
+
+require_relative '../../entity/person/person'
+
+# singleton class to create a formatted {String} with the attribute names of
+# of the submitted class
+class AttributeStringFactory
+
+  # method to return the formatted {String} for the class of the given object
+  # @param [Person] person a person or a child class of person
+  # @return [String] the formatted string with the attributes
+  # @raise [TypeError] if no case of the given class of the object is found
+  def self.get_attributes_to_person(person)
+    case person
+      when Person::Student then return "Name:;Id:;Mat.-Nr.:"
+      when Person::Person then return "Name:;Id:"
+    else
+      raise TypeError, "Invalid class type for factory: #{person.class}"
+    end
+  end
+
+  # method to return the formatted {String} for the task attributes
+  # @return [String] the formatted string with the attributes of the task
+  def self.get_attributes_to_task
+    "Task-Id;Description;Start;End"
+  end
+
+end
