db26019e0dff2e1fd0765f02a52a3720b51f263b
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-05-08 15:01:05 +0200

    added documentation to csv_writer

--- lib/output/csv/csv_writer.rb
+++ lib/output/csv/csv_writer.rb
@@ -1,13 +1,20 @@
 # @Author: Benjamin Held
 # @Date:   2016-04-18 15:41:13
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-05-04 17:48:59
+# @Last Modified time: 2016-05-08 15:00:08
 
 require 'csv'
 require_relative 'attribute_string_factory'
 
+# Singleton class to write the provided output in a csv-formatted file
 class CSVWriter
 
+  # public entry point to create the csv file
+  # @param [String] filename the path of the output file
+  # @param [Person] person the person whose tasks should be written
+  # @param [Array] tasks the result tasks
+  # @param [Array] additions additonal lines which should follow the tasks in
+  #    the csv output
   def self.output(filename, person, tasks, additions)
     output = Array.new()
 
@@ -30,6 +37,9 @@ class CSVWriter
 
   private
 
+  # method to print the output into the file
+  # @param [String] filename the path of the output file
+  # @param [Array] output the prepared output lines
   def self.write(filename, output)
     CSV.open(filename, "wb", {:col_sep => ";"}) { |csv|
       output.each { |entry|
