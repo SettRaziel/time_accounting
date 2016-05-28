32df32a7e5894efaf8064a36260bfd8dff421fc1
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-05-28 09:54:21 +0200

    adjusted readme path

--- create_yard.sh
+++ create_yard.sh
@@ -2,6 +2,6 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-30 09:48:37
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-09-21 13:14:55
+# @Last Modified time: 2016-05-28 09:54:05
 
-exec yard doc --private --readme ../README.md
+exec yard doc --private --readme ./README.md
