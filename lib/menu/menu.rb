4aa9160dab13195d0357f473d6b7d681b3268ef3
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-08-02 09:12:35 +0200

    redesigned base menu class

--- lib/menu/menu.rb
+++ lib/menu/menu.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-24 12:43:15
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-05-22 15:31:48
+# @Last Modified time: 2016-08-02 09:05:26
 
 # This module holds the classes for the terminal menu, which can be used to
 # run this program in a terminal window
@@ -15,45 +15,77 @@ module Menu
   end
 
   # This class provides the common methods of the different query menus
-  # The children need to define the method {Menu::Base.print_menu_items}. If
-  # the child class does not implement this method {Menu::Base} raises a
-  # NotImplementedError
+  # The children need to define the method {Menu::Base.define_menu_items} and
+  # {Menu::Base.determine_action}. If the child class does not implement this
+  # method {Menu::Base} raises a NotImplementedError.
   class Base
 
-    private
+    # initialization
+    # @param [String] description the headline of the menu
+    def initialize(description=nil)
+      @menu_items = Hash.new()
+      if (description == nil)
+        @menu_description = 'Default menu. Please add description: '
+      end
+      define_menu_items
+      @menu_items = Hash[@menu_items.sort]
+    end
 
-    # method to print the menu corresponding to the child class
-    # @param [String] input_string the input string which should be displayed
-    #    for the promt line
-    def self.print_menu(input_string)
-      is_not_finished = true
-      while (is_not_finished)
-        begin
-          print_menu_items
-          input = get_entry(input_string).to_i
+    # method to print the menu to the terminal and wait for input
+    def print_menu
+      isFinished = true
+      while (isFinished)
+        puts @menu_description
+        @menu_items.each_pair { |key, value|
+          puts "(#{key}) #{value}"
+        }
 
-          is_not_finished = process_input(input)
-        rescue NotImplementedError => e
-          puts e.message
-          Menu::exit_script
-        rescue StandardError => e
-          puts "Error in #{self.name.split('::').last}: ".concat(e.message).red
-        end
+        isFinished = determine_action(get_entry('Select option: '))
       end
     end
 
+    private
+
+    # @return [Hash] a hash which maps (number => string) for the menu items
+    attr :menu_items
+    # @return [String] a string with the head description of the menu
+    attr :menu_description
+
     # @abstract subclasses need to implement this method
     # @raise [NotImplementedError] if the subclass does not have this method
-    def self.print_menu_items
-      fail NotImplementedError, " Error: the subclass "
+    def define_menu_items
+      fail NotImplementedError, " Error: the subclass " \
         "#{self.name.split('::').last} needs to implement the method: " \
-        "print_menu_items from its base class".red
+        "define_menu_items from its base class".red
+    end
+
+    # @abstract subclasses need to implement this method
+    # @param [String] input the provided user input
+    # @raise [NotImplementedError] if the subclass does not have this method
+    def determine_action(input)
+      fail NotImplementedError, " Error: the subclass " \
+        "#{self.name.split('::').last} needs to implement the method: " \
+        "determine_action from its base class".red
+    end
+
+    # default behavior when a user provides not valid input
+    def handle_wrong_option
+      print 'Option not available. '.red
+      determine_action(get_entry('Select option: '))
+    end
+
+    # method to add a given key/value pair to the menu hash
+    # @param [String] description the description of the menu item
+    # @param [Integer] index the index that should be used as key
+    def add_menu_item(description, index=nil)
+      index = @menu_items.length + 1 if (index == nil)
+      @menu_items[index] = description
     end
 
     # method to print a given message and read the provided input
     # @param [String] message output message
     # @return [String] the input from the terminal
-    def self.get_entry(message)
+    def get_entry(message)
       print message.blue.bright
       gets.chomp
     end
