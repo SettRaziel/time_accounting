49e3c6fd4130207a81d37c829886de599abe536d
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-08-10 12:41:02 +0200

    redesigned custom time menu class

--- lib/menu/worktime/customtime_menu.rb
+++ lib/menu/worktime/customtime_menu.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-05-13 08:59:38
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-07-02 15:22:01
+# @Last Modified time: 2016-08-10 12:40:11
 
 module Menu
 
@@ -9,20 +9,19 @@ module Menu
   # queries based on the time interval that is presented
   module TimeMenu
 
-    # singleton class to present the available query options for a given month
+    # class to present the available query options for a given month
     # and for a given entity, identified by its id
     class CustomtimeMenu < TimeMenu
 
-      # main entry point to start a query on a person or task
-      def self.worktime_query_menu
-        @time_string = 'custom interval'
-        print_menu('Input (1-5): ')
+      # initialization
+      def initialize
+        super('custom interval')
       end
 
       private
 
       # method to retrieve the required input values
-      def self.get_input_values
+      def get_input_values
         @values = Hash.new()
         @values[:id] = get_entry('Worktime for which ID? ').to_i
         @values[:start_time] = Menu.parse_date(
@@ -35,14 +34,14 @@ module Menu
       # the custom time interval for the provided id
       # @return [Hash] a hash mapping (task_type => Array) holding the queried
       #   tasks
-      def self.retrieve_tasks
+      def retrieve_tasks
         Query::TimeQuery.retrieve_tasks(@values[:id], @values[:start_time],
                                         @values[:end_time])
       end
 
       # method to retrieve the overall worktime for an entity with the given id
       # for the custom time interval
-      def self.retrieve_worktime
+      def retrieve_worktime
         Query::TimeQuery.get_time_worktime(@values[:id], @values[:start_time],
                                            @values[:end_time])
       end
