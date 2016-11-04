733e770690f198d3c5a635b6b3ab6d2ba28fa324
Benjamin Held <me@bheld.eu>
2016-11-04 20:10:29 +0100

    added simple class to create db schema

---
+++ lib/data/sqlite/db_creator.rb
@@ -0,0 +1,43 @@
+# @Author: Benjamin Held
+# @Date:   2016-10-29 16:25:44
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2016-11-04 20:09:29
+
+module Database
+
+  class DBCreator
+
+    attr_reader :db
+
+    def initialize(db_path)
+      create_database(db_path)
+    end
+
+    def create_database(db_path)
+      begin
+        @db = SQLite3::Database.open(db_path)
+        create_tables
+      rescue IOError | StandardError
+        raise ArgumentError, "Error [DBCreator]: invalid path to database."
+      end
+    end
+
+    private
+
+    def create_tables
+      @db.execute("CREATE TABLE IF NOT EXISTS Persons(Id INTEGER PRIMARY KEY,
+                   Name TEXT)")
+      @db.execute("CREATE TABLE IF NOT EXISTS Students(Id INTEGER PRIMARY KEY,
+                   P_Id INTEGER, Mat_Nr INTEGER,
+                  FOREIGN KEY(P_Id) REFERENCES Persons(Id))")
+      @db.execute("CREATE TABLE IF NOT EXISTS Tasks(Id INTEGER PRIMARY KEY,
+                   Start DATE, End DATE, Description TEXT)")
+      @db.execute("CREATE TABLE IF NOT EXISTS Matching(Id INTEGER PRIMARY KEY,
+                   P_Id INTEGER, T_Id INTEGER,
+                   FOREIGN KEY(P_Id) REFERENCES Persons(Id),
+                   FOREIGN KEY(T_Id) REFERENCES Tasks(Id))")
+    end
+
+  end
+
+end
