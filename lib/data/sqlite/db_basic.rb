7e3f9cbc86b9615a0cf9e42fa9a81a96582cffbf
Benjamin Held <me@bheld.eu>
2016-11-09 20:20:35 +0100

    expanded db_basic commands

--- lib/data/sqlite/db_basic.rb
+++ lib/data/sqlite/db_basic.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-10-29 16:25:44
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-11-08 19:55:03
+# @Last Modified time: 2016-11-09 20:14:12
 
 module Database
 
@@ -17,20 +17,35 @@ module Database
       begin
         @db = SQLite3::Database.open(db_path)
         create_tables
-      rescue IOError | StandardError
+      rescue IOError || StandardError
         raise ArgumentError, "Error [DBCreator]: invalid path to database."
       end
     end
 
-    def insert_person(name)
-      stmt = @db.prepare("INSERT INTO Persons(Name) VALUES (?)")
-      stmt.execute(name)
+    def insert_person(id, name)
+      stmt = @db.prepare("INSERT INTO Persons(Id, Name) VALUES (?, ?)")
+      stmt.execute(id, name)
     end
 
-    def insert_task(start_time, end_time, description)
-      stmt = @db.prepare("INSERT INTO Tasks(Start, End, Description) " \
-                         " VALUES (?, ?, ?)")
-      stmt.execute(start_time.iso8601, end_time.iso8601, description)
+    def insert_task(id, start_time, end_time, description)
+      stmt = @db.prepare("INSERT INTO Tasks(Id, Start, End, Description) " \
+                         "VALUES (?, ?, ?, ?)")
+      stmt.execute(id, start_time.iso8601, end_time.iso8601, description)
+    end
+
+    def query_task(id)
+      stmt = @db.prepare("SELECT * FROM Tasks WHERE Id = ?")
+      stmt.execute(id)
+    end
+
+    def query_person_by_id(id)
+      stmt = @db.prepare("SELECT * FROM Persons WHERE Id = ?")
+      stmt.execute(id)
+    end
+
+    def query_person_by_name(name)
+      stmt = @db.prepare("SELECT * FROM Persons WHERE Name = ?")
+      stmt.execute(name)
     end
 
     private
