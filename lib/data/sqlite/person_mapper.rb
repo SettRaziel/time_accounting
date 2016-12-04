d933b22fdd42aa37bf1bfb2bad3102ca30dcd1e4
Benjamin Held <me@bheld.eu>
2016-12-04 18:30:10 +0100

    added documentation to person_mapper

--- lib/data/sqlite/person_mapper.rb
+++ lib/data/sqlite/person_mapper.rb
@@ -1,19 +1,24 @@
 # @Author: Benjamin Held
 # @Date:   2016-11-25 19:47:28
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-11-30 20:58:56
+# @Last Modified time: 2016-12-04 18:27:07
 
 require_relative '../../entity/person/person'
 require_relative 'db_basic'
 
 module DBMapping
 
+  # Class to apply ER-mapping for {Person:Person} objects to a sqlite database
   class PersonMapper
 
+    # initialization
+    # @param [String] filepath the path to the database
     def initialize(filepath)
       @db_base = Database::DBBasic.new(filepath)
     end
 
+    # public method to transform database persons to entity {Person:Person}
+    # @return [Array] all transformed {Person:Person} entities
     def generate_persons
       results = @db_base.query_persons
       persons = Array.new()
@@ -23,14 +28,18 @@ module DBMapping
       return persons
     end
 
+    # method to persist a list of {Person:Person}s to the database
+    # @param [Array] persons the {Person:Person} that should be be persisted
     def persist_persons(persons)
       persons.each { |person|
         @db_base.insert_person(person.id, person.name)
       }
+      nil
     end
 
     private
 
+    # @return [DBBasic] the basic database adapter
     attr_reader :db_base
 
   end
