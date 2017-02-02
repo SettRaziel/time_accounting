c9646c67d2966d38860b4b6356f94242ae7c223d
Benjamin Held <me@bheld.eu>
2017-02-02 21:48:04 +0100

    added parent class for db mapper

---
+++ lib/data/sqlite/base_mapper.rb
@@ -0,0 +1,35 @@
+# @Author: Benjamin Held
+# @Date:   2017-02-02 21:27:19
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2017-02-02 21:47:28
+
+module DBMapping
+
+  # This class serves as a parent class for the mapper classes to store common
+  # methods and attributes
+  class Base
+
+    # initialization
+    # @param [SQLite3::Database] database a reference of the database
+    def initialize(filepath)
+      @db_base = SqliteDatabase::DBBasic.new(filepath)
+    end
+
+    private
+
+    # @return [DBBasic] the basic database adapter
+    attr_reader :db_base
+
+    # helper method to determine the maximam id of the given database table
+    # @param [ResultSet | nil] result the query result with the given max id
+    def check_max_id(result)
+      if (result != nil)
+        Integer(result['Id'])
+      else
+        0
+      end
+    end
+
+  end
+
+end
