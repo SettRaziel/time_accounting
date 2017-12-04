cf395484daddf9778bb0ac3ce01f6901f964d457
Benjamin Held <me@bheld.eu>
2017-12-04 19:09:41 +0100

    updated yard generation

--- create_yard.sh
+++ create_yard.sh
@@ -2,6 +2,8 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-30 09:48:37
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-05-28 09:54:05
+# @Last Modified time: 2017-12-04 19:07:42
 
-exec yard doc --private --readme ./README.md
+yard doc -q --private --readme ./README.md
+printf "Looking for undocumented passages: \n"
+yard stats --list-undoc
