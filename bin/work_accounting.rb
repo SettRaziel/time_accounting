2726b6dfe9b99e964a929a3e05c07d7cbb8871b7
Benjamin Held <me@bheld.eu>
2023-01-17 21:31:56 +0100

    rework start script

--- bin/work_accounting.rb
+++ bin/work_accounting.rb
@@ -1,4 +1,3 @@
-
 require_relative '../lib/menu/menu'
 require_relative '../lib/parameter/parameter_repository'
 require_relative '../lib/output/help_output'
@@ -14,13 +13,20 @@ end
 # Version 0.3.0
 # created by Benjamin Held, September 2015
 
-if (ARGV.size > 0)
-  parameters = Parameter::ParameterRepository.new(ARGV)
-  if (parameters.parameters[:help])
-    HelpOutput.print_help_for(parameters.parameters[:help])
-  elsif (parameters.parameters[:version])
-    print_version
+begin
+  if (ARGV.size > 0)
+    parameters = Parameter::ParameterRepository.new(ARGV)
+    if (parameters.parameters[:help])
+      HelpOutput.print_help_for(parameters.parameters[:help])
+    elsif (parameters.parameters[:version])
+      print_version
+    end
+  else
+    Menu::MainMenu.new.print_menu
   end
-else
-  Menu::MainMenu.new.print_menu
+
+rescue StandardError, NotImplementedError => e
+  puts "#{e.message}".red
+  puts "For help type: ruby <script> --help".green
+  nil
 end
