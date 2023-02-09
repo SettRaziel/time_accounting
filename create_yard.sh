41693c2f99c2f1a9c6ed58b896fcf7b6e5d18c7c
Benjamin Held <me@bheld.eu>
2023-02-09 19:21:08 +0100

    update doc generation to lib folder only

--- create_yard.sh
+++ create_yard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yard doc -q --private --readme ./README.md
+yardoc -q './lib/**/*.rb' --private --readme README.md
 printf "Looking for undocumented passages: \n"
-yard stats --list-undoc
+yard stats './lib/**/*.rb' --list-undoc
